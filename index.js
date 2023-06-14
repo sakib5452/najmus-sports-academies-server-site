aa01d73db4d844a1b7a46e8e6c38f5ecfc929dd7
sakib5452 <najmus191@gmail.com>
2023-06-14 10:17:19 +0600

    admin class manage

--- index.js
+++ index.js
@@ -49,6 +49,23 @@ async function run() {
             res.send(result)
         })
 
+
+        // Get all classes
+        app.get('/classes', async (req, res) => {
+            const result = await classesCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get("/classes/:email", async (req, res) => {
+            console.log(req.params.email);
+            const classes = await classesCollection
+                .find({
+                    email: req.params.email,
+                })
+                .toArray();
+            res.send(classes);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
